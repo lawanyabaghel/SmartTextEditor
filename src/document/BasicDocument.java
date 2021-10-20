76b8b05a030391623f3bd22f9186a57cf21d8a11
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-10-20 12:21:35 +0530

    Update BasicDocument.java

--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -24,10 +24,7 @@ public class BasicDocument extends Document
 	 * i.e. any upper or lower case characters a-z or A-Z.  This method completely 
 	 * ignores numbers when you count words, and assumes that the document does not have 
 	 * any strings that combine numbers and letters. 
-	 * 
-	 * Check the examples in the main method below for more information.
-	 * 
-	 * This method should process the entire text string each time it is called.  
+	 * This method processes the entire text string each time it is called.  
 	 * 
 	 * @return The number of words in the document.
 	 */
@@ -38,30 +35,18 @@ public class BasicDocument extends Document
 		// See the Module 2 support videos if you need help.
 		List<String> tokens = getTokens("[a-zA-Z]+");
 		return tokens.size();
-		
-	    //return 0; 
 	}
 	
 	/**
 	 * Get the number of sentences in the document.
-	 * Sentences are defined as contiguous strings of characters ending in an 
-	 * end of sentence punctuation (. ! or ?) or the last contiguous set of 
-	 * characters in the document, even if they don't end with a punctuation mark.
-	 * 
-	 * Check the examples in the main method below for more information.  
-	 * 
-	 * This method should process the entire text string each time it is called.  
 	 * 
 	 * @return The number of sentences in the document.
 	 */
 	@Override
 	public int getNumSentences()
-	{
-	    //TODO: Implement this method.  See the Module 2 support videos 
-        // if you need help.
+	{ 
 		List<String> tokens = getTokens("[^.!?]+");
 		return tokens.size();
-        //return 0;
 	}
 	
 	/**
@@ -72,27 +57,17 @@ public class BasicDocument extends Document
 	 *       is not considered a syllable unless the word has no other syllables. 
 	 *       You should consider y a vowel.
 	 *       
-	 * Check the examples in the main method below for more information.  
-	 * 
-	 * This method should process the entire text string each time it is called.  
-	 * 
 	 * @return The number of syllables in the document.
 	 */
 	@Override
 	public int getNumSyllables()
 	{
-	    //TODO: Implement this method in week 2.  See the Module 2 support videos 
-        // if you need help.  And note that there is no need to use a regular
-		// expression for the syllable counting.  We recommend you implement 
-		// the helper function countSyllables in Document.java using a loop, 
-		// and then call it here on each word.
 		List<String> tokens = getTokens("[aeiouyAEIOUY]+");
 		List<String> loneEs = getTokens("[^aeiouyAEIOUY]+[eE]\\b");
 		List<String> singleEs = getTokens("\\b[^aeiouyAEIOUY]*[eE]\\b");
 		
 		
 		return tokens.size() - (loneEs.size() - singleEs.size());
-       // return 0;
 	}
 	
 	
