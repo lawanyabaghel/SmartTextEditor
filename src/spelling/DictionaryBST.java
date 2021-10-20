d2384f4d6d88ad77d2fc6c9a75abc4c5d5e02f54
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-10-20 13:53:42 +0530

    Update DictionaryBST.java

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -11,14 +11,12 @@ public class DictionaryBST implements Dictionary
    private TreeSet<String> dict;
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
    public DictionaryBST() {
 	   dict = new TreeSet<String>(); 
    }
 	
     
-    /** Add this word to the dictionary.  Convert it to lowercase first
-     * for the assignment requirements.
+    /** Add this word to the dictionary.  
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
@@ -35,7 +33,6 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
         return dict.size();
     }
 
