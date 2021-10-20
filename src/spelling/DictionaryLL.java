4b37371ff0555ebf6af70d16cc308b225e262ca6
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-10-20 13:58:51 +0530

    Update DictionaryLL.java

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -11,7 +11,6 @@ public class DictionaryLL implements Dictionary
 
 	private LinkedList<String> dict;
 	
-    // TODO: Add a constructor
 	public DictionaryLL() {
 		this.dict = new LinkedList<String>();
 	}
@@ -21,7 +20,7 @@ public class DictionaryLL implements Dictionary
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
+     */
     public boolean addWord(String word) {
     	// TODO: Implement this method
     	if (!dict.contains(word.toLowerCase())) {
