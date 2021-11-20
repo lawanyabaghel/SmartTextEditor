9c861d9c03be30acc799a3354d7032ca7b6052b4
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-20 21:00:36 +0530

    Update MyLinkedList.java

--- src/textgen/MyLinkedList.java
+++ src/textgen/MyLinkedList.java
@@ -2,19 +2,10 @@ package textgen;
 
 import java.util.AbstractList;
 
-
-/** A class that implements a doubly linked list
- * 
- * @author UC San Diego Intermediate Programming MOOC team
- *
- * @param <E> The type of the elements stored in the list
- */
 public class MyLinkedList<E> extends AbstractList<E> {
 	LLNode<E> head;
 	LLNode<E> tail;
 	int size;
-
-	/** Create a new empty LinkedList */
 	public MyLinkedList() {
 		// TODO: Implement this method
 		size = 0;
@@ -24,10 +15,6 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		tail.prev = head;
 	}
 
-	/**
-	 * Appends an element to the end of the list
-	 * @param element The element to add
-	 */
 	public boolean add(E element ) 
 	{
 		if (element == null) {
@@ -43,8 +30,6 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		return true;
 	}
 
-	/** Get the element at position index 
-	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
 	public E get(int index) 
 	{
 		// TODO: Implement this method.
@@ -59,11 +44,6 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	        return current.data;
 	}
 
-	/**
-	 * Add an element to the list at the specified index
-	 * @param The index where the element should be added
-	 * @param element The element to add
-	 */
 	public void add(int index, E element ) 
 	{
 		if (element == null) {
@@ -72,7 +52,6 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		if ((index < 0 || index > size - 1) && (index != 0 || size != 0)) {
 			throw new IndexOutOfBoundsException("Invalid index input!!");
 		}
-		// TODO: Implement this method
 		LLNode<E> nNode = new LLNode<E>(element);
 		LLNode<E> current = head;
 		for(int i = 0; i<= index; i++) {
@@ -85,21 +64,10 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		size++;
 			
 	}
-
-
-	/** Return the size of the list */
-	public int size() 
-	{
-		// TODO: Implement this method
-		return size;
+	public int size() {
+	    return size;
 	}
 
-	/** Remove a node at the specified index and return its data element.
-	 * @param index The index of the element to remove
-	 * @return The data element removed
-	 * @throws IndexOutOfBoundsException If index is outside the bounds of the list
-	 * 
-	 */
 	public E remove(int index) 
 	{
 		// TODO: Implement this method
@@ -121,16 +89,8 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		
 	}
 
-	/**
-	 * Set an index position in the list to a new element
-	 * @param index The index of the element to change
-	 * @param element The new element
-	 * @return The element that was replaced
-	 * @throws IndexOutOfBoundsException if the index is out of bounds.
-	 */
 	public E set(int index, E element) 
 	{
-		// TODO: Implement this method
 		if(index<0 || index>size-1) {
 			throw new IndexOutOfBoundsException("Invalid index input!!!");
 		}
@@ -159,8 +119,6 @@ class LLNode<E>
 	LLNode<E> next;
 	E data;
 
-	// TODO: Add any other methods you think are useful here
-	// E.g. you might want to add another constructor
 	public LLNode()
 	{
 		this.data = null;
