9a18348adbd433531ac753f374074bfb2458075e
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-20 20:26:58 +0530

    Update DictionaryBST.java

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -2,24 +2,14 @@ package spelling;
 
 import java.util.TreeSet;
 
-/**
- * @author UC San Diego Intermediate MOOC team
- *
- */
 public class DictionaryBST implements Dictionary 
 {
-   private TreeSet<String> dict;
-	
-    // TODO: Implement the dictionary interface using a TreeSet.  
+   private TreeSet<String> dict; 
    public DictionaryBST() {
 	   dict = new TreeSet<String>(); 
    }
 	
     
-    /** Add this word to the dictionary.  
-     * @param word The word to add
-     * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
     	if (!dict.contains(word.toLowerCase())) {
@@ -38,7 +28,6 @@ public class DictionaryBST implements Dictionary
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
     	return (!s.isEmpty()) && dict.contains(s.toLowerCase());
     }
 
