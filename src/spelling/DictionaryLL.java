35c5a5080535636f7a88819999e3221055cea801
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-20 20:30:59 +0530

    Update DictionaryLL.java

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -2,10 +2,7 @@ package spelling;
 
 import java.util.LinkedList;
 
-/**
- * A class that implements the Dictionary interface using a LinkedList
- *
- */
+
 public class DictionaryLL implements Dictionary 
 {
 
@@ -16,8 +13,7 @@ public class DictionaryLL implements Dictionary
 	}
 
 
-    /** Add this word to the dictionary.   
-     */
+    // Add this word to the dictionary.   
     public boolean addWord(String word) {
  
     	if (!dict.contains(word.toLowerCase())) {
@@ -27,17 +23,11 @@ public class DictionaryLL implements Dictionary
         return false;
     }
 
-
-    /** Return the number of words in the dictionary */
-    public int size()
-    {
-        // TODO: Implement this method
+    public int size(){
         return dict.size();
     }
 
-    /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
     	if(dict.contains(s.toLowerCase()) && (!s.isEmpty())) {
     		return true;
     	}
